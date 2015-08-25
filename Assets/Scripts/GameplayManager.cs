a9e23f6817768a4fd77cf45fe7ba24dee0a716b7
unstablesun <unstablesun@gmail.com>
2015-08-25 16:25:57 -0700

    puzzle advance curve

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -85,8 +85,6 @@ public class GameplayManager : MonoBehaviour
 
 	void SetupPuzzle () 
 	{
-		Debug.Log ("SetupPuzzle");
-
 		FormulaFactory _formulaFactory = GameCommon.getFormulaFactoryClass();
 
 		_elaspedPuzzleTime = 0.0f;
@@ -220,4 +218,140 @@ public class GameplayManager : MonoBehaviour
 		}
 
 	}
+
+
+
+
+
+
+	
+	
+	public struct PuzzleCurveData 
+	{
+		public int minMaxThreshold { get; set; }
+		public int minRange { get; set; }
+		public int maxRange { get; set; }
+
+		public int operatorPreference { get; set; } //do we want all plus
+		public int operatorPreferenceProb { get; set; } //probability of preferred operator
+		public int sameOperatorProb { get; set; } //for 2 or more equations, probability they will be the same
+
+		public int numEquationThreshold { get; set; }
+		public int numEquations { get; set; }
+
+		public int resetRangeAfterEquationInc { get; set; }
+
+	}
+
+	private PuzzleCurveData mPuzzleCurveData;
+
+	//set once per game
+	public void SetupPuzzleCurveData (int minMaxThreshold, int minRange, int maxRange, 
+	                           int operatorPreference, int operatorPreferenceProb, int sameOperatorProb,
+	                           int numEquationThreshold, int numEquations) 
+	{
+		mPuzzleCurveData.minMaxThreshold = minMaxThreshold;
+		mPuzzleCurveData.minRange = minRange;
+		mPuzzleCurveData.maxRange = maxRange;
+
+		mPuzzleCurveData.operatorPreference = operatorPreference;
+		mPuzzleCurveData.operatorPreferenceProb = operatorPreferenceProb;
+		mPuzzleCurveData.sameOperatorProb = sameOperatorProb;
+
+		mPuzzleCurveData.numEquationThreshold = numEquationThreshold;
+		mPuzzleCurveData.numEquations = numEquations;
+	}
+
+	void AdvancePuzzleCurve () 
+	{
+		FormulaFactory _formulaFactory = GameCommon.getFormulaFactoryClass();
+
+		int minMaxThreshold = mPuzzleCurveData.minMaxThreshold;
+		int minRange = mPuzzleCurveData.minRange;
+		int maxRange = mPuzzleCurveData.maxRange;
+
+		FormulaFactory.eOperandBias operatorPref = (FormulaFactory.eOperandBias)mPuzzleCurveData.operatorPreference;
+		int operatorPreferenceProb = mPuzzleCurveData.operatorPreferenceProb;
+		int sameOperatorProb = mPuzzleCurveData.sameOperatorProb;
+
+		//int operatorType = UnityEngine.Random.Range(0, 5);//0-3
+
+		FormulaFactory.eOperandBias operand = FormulaFactory.eOperandBias.forcePlus;
+		FormulaFactory.eOperandBias firstOperand = FormulaFactory.eOperandBias.forcePlus;
+
+		int numEquationThreshold = mPuzzleCurveData.numEquationThreshold;
+		int numEquations = mPuzzleCurveData.numEquations;
+		for (int i = 0; i < numEquations; i++) 
+		{
+
+
+			bool operatorSet = false;
+			if(i>0)
+			{
+				int chanceSame = UnityEngine.Random.Range(0, 100);
+				if(chanceSame < sameOperatorProb)
+				{
+					operand = firstOperand;
+					operatorSet = true;
+				}
+			}
+
+			if(operatorSet == false)
+			{
+				if(operatorPref == FormulaFactory.eOperandBias.any)
+				{
+					operand = (FormulaFactory.eOperandBias)UnityEngine.Random.Range(1, 5);//1-4
+				}
+				else
+				{
+					int chancePref = UnityEngine.Random.Range(0, 100);
+
+					if(chancePref < operatorPreferenceProb)
+					{
+						operand = operatorPref;
+					}
+					else
+					{
+						operand = (FormulaFactory.eOperandBias)UnityEngine.Random.Range(1, 5);//1-4
+					}
+				}
+			}
+
+			if(i == 0)
+			{
+				firstOperand = operand;
+			}
+			
+
+
+			//add random select bucket
+			
+			
+			_formulaFactory.AddEquation(0, minRange, maxRange, operand );
+
+		}
+
+		//update thresholds
+		if (_gameLevel % minMaxThreshold == 0) {
+
+			//crossed threshold
+			//advance some curve data
+
+			maxRange++;
+			mPuzzleCurveData.maxRange = maxRange;
+
+		}
+
+		if (_gameLevel % numEquationThreshold == 0) {
+			
+			//crossed threshold
+			//advance some curve data
+			
+			numEquations++;
+			mPuzzleCurveData.numEquations = numEquations;
+			
+		}
+
+	}
+
 }
